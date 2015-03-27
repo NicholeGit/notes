ff8b102e3cadd33b800ae8e2b8e7de1d4da1c2fe
Nichole <123576128@qq.com>
2015-03-27 14:35:49 +0800

    提交flyweight（享元模式）

---
+++ golang/design_pattern/src/flyweight/flyweight_test.go
@@ -0,0 +1,17 @@
+package flyweight
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFlyWeight(t *testing.T) {
+	bigStr := NewBigString("121")
+	result := bigStr.Print()
+
+	expect := "-\n--\n-\n"
+	if result != expect {
+		t.Errorf("Expect result to equal %s, but %s.\n", expect, result)
+	}
+	fmt.Println(bigStr)
+}
